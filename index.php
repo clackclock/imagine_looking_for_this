2fcbc10f5b5e042935710c2a59a63087578a8cb1
Cluck <143739151+clackclock@users.noreply.github.com>
2023-12-13 09:52:19 -0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Climate+Crisis&display=swap"
+      rel="stylesheet"
+    />
+<title>The Cluck Has Landed</title>
+</head>
+    <style>
+        body{
+            background-color: #c7b591;
+            background-image: url('/sacia.matthews/social_software_blog/images/lake.jpg');
+            background-size: cover;
+            background-position: top;
+            
+        }
+        footer{
+            color: #436a7b;
+            font-family: Climate Crisis;
+        }
+        h1{
+            /* font-size: 30px; */
+            color:#436a7b;
+            font-family: Climate Crisis;
+        }
+        
+    </style>
+<body>
+    <h1>S.Matthews AKA Cluck AKA The Biggest 's Landing Page </h1>
+    <a href="/sacia.matthews/now/">Now Page</a>
+    
+
+</body>
+<footer>
+<div id="social">
+			<script
+				type="text/javascript"
+				src="/lee.tusman/assets/js/onionring-variables.js"
+			></script>
+			<script
+				type="text/javascript"
+				src="/lee.tusman/assets/js/onionring-widget.js"
+			></script>
+    <h4>
+        This site is part of the Social Software Webring
+    </h4>
+	</div>
+</footer>
+<HTML>
