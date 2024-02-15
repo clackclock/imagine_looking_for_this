30ebf3999c6fffb56f8c99c1f919a7509a381bab
clackclock <clackclick2020@gmail.com>
2024-02-14 19:14:17 -0500

    added images and new folder

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+$(document).ready(function(){ //when the page loads do this
+
+    $("#img").hover(function(){ //when clicking "" do this
+        $("#img").css("background-color", "#800080");
+    })
+})
--- scriptWeb/script.js
+++ scriptWeb/script.js
@@ -1,1 +1,5 @@
-
+$(document).ready(function(){
+    $("footer").click(function(){
+        $("body").css("background-image","https://i.pinimg.com/originals/1c/aa/a6/1caaa6739233c724befe59ed743c4413.gif");
+    })
+})
