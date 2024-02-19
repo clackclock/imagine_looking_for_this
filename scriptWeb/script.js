e1dc0129594db11865842a8bdf38cdcfecbb81eb
Cluck <143739151+clackclock@users.noreply.github.com>
2024-02-18 21:30:23 -0500

    Update script.js

--- scriptWeb/script.js
+++ scriptWeb/script.js
@@ -4,6 +4,8 @@
 //     })
 // })
 
+setInterval(altPage, 500);
+
 function altPage(){
     document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/1c/aa/a6/1caaa6739233c724befe59ed743c4413.gif')";
     document.body.style.color = rgb(255,0,0);
