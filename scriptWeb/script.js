a8b8745ff442e8747d357b4c20c67f423960fbcf
Cluck <143739151+clackclock@users.noreply.github.com>
2024-02-18 21:22:04 -0500

    Update script.js

--- scriptWeb/script.js
+++ scriptWeb/script.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-    $("footer").click(function(){
-        $("body").css("background-image","https://i.pinimg.com/originals/1c/aa/a6/1caaa6739233c724befe59ed743c4413.gif");
-    })
-})
+// $(document).ready(function(){
+//     $("footer").click(function(){
+//         $("body").css("background-image","https://i.pinimg.com/originals/1c/aa/a6/1caaa6739233c724befe59ed743c4413.gif");
+//     })
+// })
+
+function altPage(){
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/1c/aa/a6/1caaa6739233c724befe59ed743c4413.gif')";
+    document.body.style.color = rgb(255,0,0);
+}
